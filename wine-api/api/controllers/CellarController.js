e4c9b21f920a70479304f56e0c85274359b202cc
merry31 <julien.rallo@gmail.com>
2016-04-20 17:37:37 +0200

    Wine & Cellar skeleton

---
+++ wine-api/api/controllers/CellarController.js
@@ -0,0 +1,11 @@
+/**
+ * CellarController
+ *
+ * @description :: Server-side logic for managing cellars
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ wine-api/api/controllers/WineController.js
@@ -0,0 +1,11 @@
+/**
+ * WineController
+ *
+ * @description :: Server-side logic for managing wines
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
